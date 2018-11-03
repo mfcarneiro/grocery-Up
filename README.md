cf94952f61588b28187e76618204ad029b863e6a
Nero <matheus.felipe123@gmail.com>
2018-11-03 20:02:06 -0300

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GroceryUp
+
+- Will be updated in the future to run in an internal network to build a grocery list, stopping my wife complaining for food requests that don't blow the budget :laughing:
---
+++ main.ts
@@ -0,0 +1,57 @@
+const submissionComponent = {
+  template: `
+        <div style="display: flex; width: 100%">
+          <figure class="media-left">
+            <img class="image is-64x64" :src="submission.submissionImage">
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>
+                  <a href="submission.url" class="has-text-info">{{ submission.title }}</a>
+                  <span class="tag is-small">#{{ submission.id }}</span>
+                </strong>
+                <br>
+                {{ submission.description }}
+                <br>
+                <small class="is-size-7">
+                  Submitted by:
+                  <img class="image is-24x24" :src="submission.avatar">
+                </small>
+              </p>
+            </div>
+          </div>
+          <div class="media-right">
+            <span class="icon is-small">
+              <i class="fa fa-chevron-up"></i>
+              <strong class="has-text-info" @click="upVote(submission.id)">{{ submission.votes}}</strong>
+            </span>
+          </div>
+        </div>`,
+  props: ['submission', 'submissions'],
+  methods: {
+    upVote(submissionId) {
+      const submission = this.submissions.find(
+        submission => submission.id === submissionId
+      );
+      submission.votes++;
+    }
+  }
+};
+
+new Vue({
+  el: "#app",
+  components: {
+    'submission-component': submissionComponent
+  },
+  data: {
+    submissions: Seed.submissions
+  },
+  computed: {
+    sortedSubmissions() {
+      return this.submissions.sort((a, b) => {
+        return b.votes - a.votes;
+      });
+    }
+  }
+});
